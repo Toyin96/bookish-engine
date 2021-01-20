84eec01c33e92193e963857e98f6bb5de0cab209
Toyin96 <onagoruwam@gmail.com>
2021-01-20 23:29:47 +0100

    My first Python script on github

---
+++ firstPythonProgram.py
@@ -0,0 +1,16 @@
+# My first Python Code
+
+"""This is a basic comparison script using two numbers"""
+
+firstNum = int(input('Enter first number: '))
+
+secondNum = int(input('Enter first number: '))
+
+if firstNum < secondNum:
+	print(firstNum, ' is lesser than', secondNum)
+
+if firstNum == secondNum:
+	print(firstNum, ' is equal to', secondNum)	
+
+firstNum != secondNum:
+	print(firstNum, ' is not equal to', secondNum)
