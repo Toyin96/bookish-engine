3e4cacaace99a3da941ceb304e9698cb9fd7e8e1
Toyin96 <onagoruwam@gmail.com>
2021-01-21 07:37:41 +0100

    Chapter 2 exercises (Multiples, Odd & Even program)

---
+++ Chaper2 Exercises/OddOrEven.py
@@ -0,0 +1,10 @@
+# Exercise 2.6
+""" Program to check is an integer is odd or even"""
+
+a = int(input('Enter an integer: '))
+
+if a % 2 == 0:
+    print(a, 'is an even number')
+    
+if a % 2 != 0:
+    print(a, 'is an odd number')
