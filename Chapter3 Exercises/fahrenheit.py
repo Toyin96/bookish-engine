f58bbe9a2fcac11c1b8af8b3643906eca04c14b1
Toyin96 <onagoruwam@gmail.com>
2021-01-31 09:39:48 +0100

    A proram that can convert celsius values to fahrenheit

---
+++ Chapter3 Exercises/fahrenheit.py
@@ -0,0 +1,15 @@
+def fahrenheit(arg1: float) -> float:
+    """
+    This function takes in one argument (i.e a degree value in celsius) and
+    converts the number into its its corresponding degree in fahrenheit
+
+    Returns
+    -------
+    the degree value in fahrenheit
+
+    """
+
+    return (9 / 5) * arg1 + 32
+
+
+print(fahrenheit(23)) 
