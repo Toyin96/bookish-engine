add5abeaa35bfeeb15f64f2763143677336ac108
Toyin96 <onagoruwam@gmail.com>
2021-02-05 00:05:50 +0100

    random number modified version

---
+++ Chapter4 Exercises/random_game_engine_modified.py
@@ -0,0 +1,31 @@
+def random_game_engine():
+    """This function runs a guess game engine where the user
+    is expected guess the random number correctly
+    by choosing a number between 1 and 1000."""
+
+    print('Welcome to random game engine...a place where you put your guessing abilities into great test. \n')
+    random_number = 1 + random.randrange(1000)
+    reply = int(input('Guess my number between 1 and 1000 with the fewest guesses:'))
+    counter = 0
+    while reply != random_number:
+        random_number = 1 + random.randrange(1000)
+        reply = int(input('Guess my number between 1 and 1000 with the fewest guesses:'))
+        counter += 1
+        if reply > random_number:
+            print('Too high. Try again.')
+        elif reply < random_number:
+            print('Too low. Try again.')
+        else:
+            print('Congratulations. You guessed the number!')
+            if counter < 10:
+                print('Either you know the secret or you got lucky!')
+            else:
+                print('You should be able to do better!')
+            user_reply = int(input('Would you like to continue this beautiful game? Enter 1 for yes, 2 for no'))
+            if user_reply == 1:
+                random_game_engine()
+            else:
+                print('Thank you for your time. see ya...')
+
+
+random_game_engine()
